a7c0693a6fd0f4e3d7c86f461777fd2cbbc9775a
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-23 09:30:32 +0800

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import './reset.css';
+import './index.css';
+
+export default function App() {
+  return <></>;
+}
+
+createRoot(document.getElementById('root')!).render(<App />);

85981a979213e0fcf220f9d1712c0658c0c89a0a
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-23 14:25:47 +0800

    define main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import './reset.css';
 import './index.css';
+import Card from './components/Card/Card';
 
 export default function App() {
-  return <></>;
+  return (
+    <main>
+      <Card />
+    </main>
+  );
 }
 
 createRoot(document.getElementById('root')!).render(<App />);

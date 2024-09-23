30d42ee3ee297f127855059eec993d9c4db9a388
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-23 22:31:07 +0800

    define Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,35 +1,59 @@
 import styles from './Card.module.css';
-import avatar from '../../assets/images/avatar-jessica.jpeg';
+import avatar from '../../assets/images/photoshoot.jpg';
+import { useState } from 'react';
+import { FiSun } from 'react-icons/fi';
+import { FaRegMoon } from 'react-icons/fa';
 
 export default function Card() {
+  const [isLightTheme, setIsLightTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsLightTheme(!isLightTheme);
+  };
+
+  const themeClass = isLightTheme ? styles.themeLight : '';
+
   return (
-    <div className={styles.card}>
-      <img
-        className={styles.avatar}
-        src={avatar}
-        alt='Avatar image of a person'
-      />
-      <h1 className={styles.name}>Jessica Randall</h1>
-      <p className={styles.location}>London, United Kingdom</p>
-      <p className={styles.description}>
-        "Front-end developer and avid reader."
-      </p>
-      <div className={styles.links}>
-        <a className={styles.link} href=''>
-          GitHub
-        </a>
-        <a className={styles.link} href=''>
-          Frontend Mentor
-        </a>
-        <a className={styles.link} href=''>
-          LinkedIn
-        </a>
-        <a className={styles.link} href=''>
-          Twitter
-        </a>
-        <a className={styles.link} href=''>
-          Instagram{' '}
-        </a>
+    <div className={`${styles.bodyContainer} ${themeClass}`}>
+      <div className={`${styles.card} ${themeClass}`}>
+        <div className={styles.profileContainer}>
+          <img
+            className={styles.avatar}
+            src={avatar}
+            alt='Avatar image of a person'
+          />
+          <button
+            onClick={toggleTheme}
+            className={`${styles.toggleButton} ${themeClass}`}
+          >
+            {isLightTheme ? (
+              <FaRegMoon />
+            ) : (
+              <FiSun style={{ color: 'white' }} />
+            )}
+          </button>
+        </div>
+        <h1 className={`${styles.name} ${themeClass}`}>Ronald Yonggi</h1>
+        <p className={`${styles.location} ${themeClass}`}>
+          Queens, New York
+        </p>
+        <p className={`${styles.description} ${themeClass}`}>
+          "Full stack developer, DevOps engineer, and breakdancer in training."
+        </p>
+        <div className={styles.links}>
+          <a className={styles.link} href='https://github.com/ronaldyonggi' target='_blank'>
+            GitHub
+          </a>
+          <a className={styles.link} href='https://www.frontendmentor.io/profile/ronaldyonggi' target='_blank'>
+            Frontend Mentor
+          </a>
+          <a className={styles.link} href='https://www.linkedin.com/in/ronaldyonggi/' target='_blank'>
+            LinkedIn
+          </a>
+          <a className={styles.link} href='https://x.com/yonggron' target='_blank'>
+            X / Twitter
+          </a>
+        </div>
       </div>
     </div>
   );
